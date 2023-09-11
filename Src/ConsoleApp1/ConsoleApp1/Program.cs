1a5eef17866139bae0ff9dec660461b81f743118
fred <domaine75@gmail.com>
2023-09-11 07:01:42 +0200

    create a project and a solution

---
+++ Src/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+  internal class Program
+  {
+    static void Main()
+    {
+      var soundexPtBr = Fonetico.Fonetiza("João de Deus");
+      Console.WriteLine(soundexPtBr.Normalize());
+
+      Console.WriteLine("press any key to exit:");
+      Console.ReadKey();
+    }
+  }
+}
